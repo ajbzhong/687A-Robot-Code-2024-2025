3dd384cf04240a19de84c344f6663725c4857bd4
Alice Zhong <azhong377@gmail.com>
2024-09-19 16:26:00 -0700

    drivetrain

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/*---------GLOBALS----------*/
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -74,9 +79,6 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
@@ -85,10 +87,26 @@ void opcontrol() {
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		// int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		// int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		// left_mg.move(dir - turn);                      // Sets left motor voltage
+		// right_mg.move(dir + turn);                     // Sets right motor voltage
+		// pros::delay(20);                               // Run for 20 ms then update
+
+		// tank drive
+		// DRIVEBASE
+		int left = master.get_analog(ANALOG_LEFT_Y); //joystick
+		int right = master.get_analog(ANALOG_RIGHT_Y); //joystick
+		
+		//left joystick deadzone
+		if(left < 10 && left > -10){
+			left = 0;
+		}
+		left_mg.move(left);
+		//right joystick deadzone
+		if(right < 10 && right > -10){
+			right = 0;
+		}
+		right_mg.move(right);
 	}
 }
